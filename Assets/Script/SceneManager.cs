bdb8782ae36b29787427413bd5004fdf60f56d94
Patcharaphon Santhitikul <bentorrrrr@gmail.com>
2024-11-28 02:37:17 +0700

    Adding mainmenu + Finsing Level Canvas

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("Loading Scene: " + sceneName);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void ReloadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
